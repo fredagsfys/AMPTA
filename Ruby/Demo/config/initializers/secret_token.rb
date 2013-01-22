40afc2da7e74fba404c9af39cbdaaa2fafdfe7f9
Haris <hk222cx@student.lnu.se>
2013-01-22 14:01:14 +0100

    Map structure
    Creating a good map structure

---
+++ Ruby/Demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'e768bbda033794d346328485c56743f4d38a71d7c892995d2de7545d24ce9924221bcab89219c34d935f664102a6a7d5242efc76a705284eadb7e121b4b455df'
