7c8d5d2a6e9a98a4b26a643ad07cf444abbc8ecc
Haris <hk222cx@student.lnu.se>
2013-01-22 14:51:49 +0100

    AMPTA v0.1
    Created new project.

---
+++ Ruby/AMPTA/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AMPTA::Application.config.secret_token = '4af0d1b423a5d07135cea81c305ee7a589a66252018f32b2f643fd5d4dd142d29d07f99fbf7614c3aa7860e6cfbef8fb61bcfb9f6b63f447790dd93316d2f205'
